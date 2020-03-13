edeb279c9ac14db8d1a0e55f08e2d170f798e3b7
Gregorio <Gregorio@DESKTOP-6VLKNAE>
2020-03-13 00:43:47 -0300

    Inicio do projeto

---
+++ Arrependeivos/ArrependeivosWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ArrependeivosWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
